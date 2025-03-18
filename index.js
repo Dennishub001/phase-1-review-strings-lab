9bacefe927841aca2235879f5322b61f8b70fba6
dennisochieng <ochiengdennis32@gmail.com>
2025-03-18 21:23:30 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code in this file!
+const currentUser =  'Dennis';
+
+const welcomeMessage =  " Welcome to Flatbook, " + currentUser + "!";
+ 
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase( "WELCOME TO FLATBOOK," + currentUser +  "!");
+
+
+const shortGreeting = "Welcome, " +  'D'+ "!"; 
+
+
